701de37922255b6eff3103d52e622859880d2c71
unknown <lucascteixeira0@gmail.com>
2025-03-19 12:06:49 -0300

    add assets, config fonts and tailwind theme variables

---
+++ src/app/utils/fonts.ts
@@ -0,0 +1,25 @@
+import { IBM_Plex_Mono, Montserrat, Space_Grotesk } from 'next/font/google'
+
+export const ibmPlexMono = IBM_Plex_Mono({
+  variable: '--font-data',
+  weight: ['400', '700'],
+  fallback: ['monospace'],
+  subsets: ['latin'],
+  style: ['normal'],
+})
+
+export const spaceGrotesk = Space_Grotesk({
+  variable: '--font-title',
+  weight: ['700'],
+  fallback: ['sans-serif'],
+  subsets: ['latin'],
+  style: ['normal'],
+})
+
+export const montserrat = Montserrat({
+  variable: '--font-body',
+  weight: ['400'],
+  fallback: ['sans-serif'],
+  subsets: ['latin'],
+  style: ['normal'],
+})
